77260659b26b636b2bb8ef231f371722b039dd35
Berkay_ilhan <berkay.ilhan@enai.eu>
2019-05-01 09:32:57 +0200

    Added a  function to increment a number evertytime the button is clicked.

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -4,20 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
+    private TextView mainText;
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btnStart = findViewById(R.id.startBtn);
+        mainText = findViewById(R.id.mainText);
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Button clicked !", Toast.LENGTH_SHORT);
+                count += 1;
+                mainText.setText("Clicked = " + count);
             }
         });
     }
