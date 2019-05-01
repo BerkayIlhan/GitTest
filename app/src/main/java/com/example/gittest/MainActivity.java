f98874a2cf879a79949c1776144a3dfe5930862f
Berkay_ilhan <berkay.ilhan@enai.eu>
2019-05-01 09:57:08 +0200

    Added background color change functionality.

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gittest;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
     private TextView mainText;
+    private ConstraintLayout mainLayout;
     private int count = 0;
 
     @Override
@@ -23,13 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart = findViewById(R.id.startBtn);
         mainText = findViewById(R.id.mainText);
+        mainLayout = findViewById(R.id.mainLayout);
+        final String[] colors = new String[]{ "#00FF00", "#008080", "#FF00FF", "#0000FF", "#FF0000"};
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Button clicked !", Toast.LENGTH_SHORT);
                 count += 1;
+                if(count == 5){
+                    count = 1;
+                }
                 mainText.setText("Clicked = " + count);
+                mainLayout.setBackgroundColor(Color.parseColor(colors[count - 1]));
             }
         });
         Log.i("Test", "Test");
